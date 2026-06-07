f7ff99e1e61f91c5e9012d036be36e1b2ed27129
nasirulla <rehmanansirulla1124@gmail.com>
2026-06-08 03:12:06 +0530

    deploy-3

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,9 +10,13 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://zoreel-3.onrender.com",
     credentials:true
 }))
+// app.use(cors({
+//     origin:"http://localhost:5173",
+//     credentials:true
+// }))
 
 
 app.use("/api/user" , userAuthRouter);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Routing from './services/routing'
+import Routing from './services/Routing'
 import Pass from '../pro/pass'
 
 
